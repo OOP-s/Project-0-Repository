24e342312b481b5c06eef80710d2f44a299cda2e
John Baker-Heal <bakejoh5@isu.edu>
2021-03-22 22:53:43 -0600

    Added fields to abstract class itemList

--- app/src/main/java/ToDoListManager/itemList.java
+++ app/src/main/java/ToDoListManager/itemList.java
@@ -2,10 +2,22 @@ package ToDoListManager;
 
 public abstract class itemList {
 
+    private String title;
+    private String description;
 
-    public static void main(String[] args) {
-        System.out.println("This is a test");
+    public itemList(String t, String d) {
+        title = t;
+        description = d;
     }
 
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public void setTitle(String newTitle) { title = newTitle; }
+    public void setDescription(String newDescription) { description = newDescription; }
+    public void sortListbyDueDate() {  }
+    public void sortListbyPriority() {  }
+    public void sortListbyLabels(String label) {  }
+    public void showCompletedTasks() {  }
+    public void removeItemList() {  }
 
 }
--- app/src/test/java/ToDoListManager/AppTest.java
+++ app/src/test/java/ToDoListManager/AppTest.java
@@ -3,9 +3,6 @@
  */
 package ToDoListManager;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class AppTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
