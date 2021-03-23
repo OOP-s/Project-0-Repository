5f3b4456a95967942f9a968628001320d536b500
John Baker-Heal <bakejoh5@isu.edu>
2021-03-22 23:03:23 -0600

    Added class project and fields to project

---
+++ app/src/main/java/ToDoListManager/project.java
@@ -0,0 +1,14 @@
+package ToDoListManager;
+
+public class project extends itemList {
+
+    protected project(String title, String description) {
+        super(title, description);
+    }
+
+    public project newProject(String title, String description) {
+       return new project(title,description);
+    }
+    public void selectProjectForDisplay() {  }
+
+}
