84690bb3a0032e0d418e0c2ef1c27a1f0e38cb9d
stougarr <stougarr@isu.edu>
2021-03-27 18:49:14 -0600

    committing class files

---
+++ app/src/main/java/ToDoListManager/UserTemplate.java
@@ -0,0 +1,11 @@
+package ToDoListManager;
+
+public abstract class UserTemplate {
+    private String email;
+    private String password;
+
+    public boolean checkLogin(String email, String password){
+        //code to fetch email.
+        return true;
+    }
+}
