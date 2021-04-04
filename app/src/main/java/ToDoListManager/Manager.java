cdffadc929bc163b131e6c4783ce1e6818ac4fe3
stougarr <stougarr@isu.edu>
2021-04-03 20:31:13 -0600

    Changed the Manager class

--- app/src/main/java/ToDoListManager/Manager.java
+++ app/src/main/java/ToDoListManager/Manager.java
@@ -5,51 +5,43 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Manager {
-    public User[] users;
-    public Admin admin;
+    public ArrayList<User> users = new ArrayList<>();
+    public Admin admin = new Admin();
 
-    //add user to the user array
-    private User[] addUser(User[] currentUsers, User newUser){
-        int currentSize = currentUsers.length;
-        User[] newUsers = new User[currentSize+1];
-        // add all other users to new array
-        for (int i=0; i<currentSize + 1; i++ ){
-            newUsers[i] = currentUsers[i];
-        }
-        // add new element
-        newUsers[currentSize + 1] = newUser;
-        return newUsers;
+    //method to register user.
+    public void registerUser(String username, String password, String firstName, String lastName) {
+        //code to read all users from gson file into the ArrayList.
+        User newUser = new User(username, password, firstName, lastName);
+        users.add(newUser);
+        //code to write all users into a new gson file.
     }
-    //function to register new user
-    public void registerUser(String username, String password, String firstName, String lastName) throws IOException {
-        User newUser =  new User(username, password, firstName, lastName);
-        users = addUser(users, newUser);
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        FileWriter writer = new FileWriter("userInfo/users");
-        writer.write(gson.toJson(users));
-        writer.close();
-
-    }
-
-    //code that runs win the user press login
-    public void userLogin(String username, String password){
-        int numUsers = users.length;
-        for (int i=0; i< numUsers; i++) {
-            User potentialUser = users[i];
-            if(potentialUser.checkLogin(username,password)){
-                //code to Login and go to users account
+    //method for logging in as a user.
+    public void loginUser(String username, String password) {
+        //code to read all users from gson file into the ArrayList.
+        int amntUsers = users.size();
+        for (int i =0; i < amntUsers; i++) {
+            if(users.get(i).getUsername() == username && users.get(i).getPassword() == password){
+                //code to login to the users account.
             }
         }
-        if (admin.checkLogin(username, password)){
-            //code to Login as admin
+        if (admin.getUsername() == username && admin.getPassword() == password) {
+            //code to login as admin.
+        }
+        else {
+            // code to display invalid login.
         }
     }
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
         Manager manager = new Manager();
-        manager.registerUser("stougarr", "password", "Garrett", "Stouffer");
+        manager.registerUser("stougarr", "passw0rd", "Garrett", "Stouffer");
+        manager.registerUser("test", "testing", "bill", "Hopkins");
+        System.out.println(manager.users.toString());
     }
+
+
 }
